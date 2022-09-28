5a117d7acbfc43594729f3948426ca5aa13c23ab
Astemir Bekov <artisticheski@gmail.com>
2022-09-28 17:40:56 +0300

    added launch guide component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import AsideBar from "./components/AsideBar/AsideBar";
 import HeaderForGuide from "./components/HeaderForGuide/HeaderForGuide";
+import LaunchGuide from "./components/LaunchGuide/LaunchGuide";
 
 function App() {
   return (
     <div className="App">
       <AsideBar/>
       <HeaderForGuide/>
+      <LaunchGuide />
     </div>
   );
 }
