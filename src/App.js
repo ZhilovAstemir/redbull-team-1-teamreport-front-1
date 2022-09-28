6a2d74a31a50eeb635b6005a8125b542e3e58619
Astemir Bekov <artisticheski@gmail.com>
2022-09-28 15:53:45 +0300

    added launch guide

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import AsideBar from "./components/AsideBar/AsideBar";
 
 function App() {
   return (
     <div className="App">
-
+      <AsideBar/>
     </div>
   );
 }
---
+++ src/redux/models/model.js
@@ -0,0 +1,15 @@
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+
+export const main = createSlice({
+  name: "main",
+  initialState,
+  reducers: {
+  //  ...
+  }
+});
+
+export const {
+//  methods
+} = main.actions;
+
+export default main.reducer;
