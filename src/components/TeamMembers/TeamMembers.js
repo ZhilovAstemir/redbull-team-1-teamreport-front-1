e69e8e14f50feab42e6d1558b8d947950064eb95
Astemir Bekov <artisticheski@gmail.com>
2022-09-29 11:30:50 +0300

    done with mycompany page

---
+++ src/components/TeamMembers/TeamMembers.js
@@ -0,0 +1,63 @@
+import React, {memo} from 'react';
+import styles from "./TeamMembers.module.css";
+
+const members = [
+  {
+    id: Math.random(),
+    name: "Aleksandr Evseev",
+  },
+  {
+    id: Math.random(),
+    name: "Nikolai Kapustin",
+  },
+  {
+    id: Math.random(),
+    name: "Anna Kotova",
+  },
+  {
+    id: Math.random(),
+    name: "Nina Mammadova",
+  },
+  {
+    id: Math.random(),
+    name: "Natalia Starkova",
+  },
+  {
+    id: Math.random(),
+    name: "Anton Tarkhanov",
+  },
+  {
+    id: Math.random(),
+    name: "Alexandr Vovchuk",
+  },
+]
+
+const TeamMembers = () => {
+  return (
+    <div className={styles.teamMembers_container}>
+      <h2 className={styles.title}>TEAM MEMBERS</h2>
+      <div className={styles.hr}></div>
+      <section className={styles.members_box}>
+        {members.map((member) => (
+          <div className={styles.members_flex}>
+            <div className={styles.members_flex_little}>
+              <div
+                className={styles.circle}>
+                {member.name.split(" ")[0][0]}
+                {member.name.split(" ")[1][0]}
+              </div>
+              <div className={styles.members_div} key={member.id}>
+                {member.name}
+              </div>
+            </div>
+            <a className={styles.btn_edit} href="">
+              <button>Edit</button>
+            </a>
+          </div>
+        ))}
+      </section>
+    </div>
+  );
+};
+
+export default memo(TeamMembers);
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {Provider} from "react-redux";
 import {store} from "./redux/store";
+import {BrowserRouter} from "react-router-dom";
 
 const container = document.getElementById('root');
 const root = createRoot(container);
@@ -12,7 +13,9 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App/>
+      <BrowserRouter>
+        <App/>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
