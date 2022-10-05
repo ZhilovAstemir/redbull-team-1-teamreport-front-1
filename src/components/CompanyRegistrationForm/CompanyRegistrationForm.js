4690a9281658fb7cd3e2312331a7df41a559d412
Efim Shikanov <darkcoderb@gmail.com>
2022-10-05 16:36:19 +0500

    added TeamMemberRegistration and refactored code

--- src/components/CompanyRegistrationForm/CompanyRegistrationForm.js
+++ src/components/CompanyRegistrationForm/CompanyRegistrationForm.js
@@ -1,4 +1,4 @@
-import React, { useState, memo } from "react";
+import React, { memo } from "react";
 import { useForm } from "react-hook-form";
 import styles from "./CompanyRegistrationForm.module.css";
 
@@ -18,20 +18,30 @@ const CompanyRegistrationForm = () => {
   return (
     <div>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <label className={styles.card__inputLabel}>Company Name</label>
+        <label className={styles.form__inputLabel}>Company Name</label>
         <input
-          className={styles.card__input}
-          {...register("companyName", { required: "Company Name is required" })}
+          className={styles.form__input}
+          {...register("companyName", {
+            required: "Company Name is required",
+            minLength: {
+              value: 3,
+              message: "Company name should be longer than 3 characters"
+            },
+            maxLength: {
+              value: 100,
+              message: "Company name should not be longer than 100 characters"
+            }
+          })}
           aria-invalid={errors.companyName ? "true" : "false"}
         />
         {errors.companyName && (
-          <p className={styles.card__validationMessage} role="alert">
+          <p className={styles.form__validationMessage} role="alert">
             {errors.companyName?.message}
           </p>
         )}
-        <label className={styles.card__inputLabel}>Email</label>
+        <label className={styles.form__inputLabel}>Email</label>
         <input
-          className={styles.card__input}
+          className={styles.form__input}
           {...register("email", {
             pattern: {
               value: emailRegexp,
@@ -42,13 +52,13 @@ const CompanyRegistrationForm = () => {
           aria-invalid={errors.email ? "true" : "false"}
         />
         {errors.email && (
-          <p className={styles.card__validationMessage} role="alert">
+          <p className={styles.form__validationMessage} role="alert">
             {errors.email?.message}
           </p>
         )}
-        <label className={styles.card__inputLabel}>Password</label>
+        <label className={styles.form__inputLabel}>Password</label>
         <input
-          className={styles.card__input}
+          className={styles.form__input}
           {...register("password", {
             pattern: {
               value: passwordRegexp,
@@ -59,12 +69,12 @@ const CompanyRegistrationForm = () => {
           aria-invalid={errors.password ? "true" : "false"}
         />
         {errors.password && (
-          <p className={styles.card__validationMessage} role="alert">
+          <p className={styles.form__validationMessage} role="alert">
             {errors.password?.message}
           </p>
         )}
 
-        <input className={styles.card__button} type="submit" value="Invite" />
+        <input className={styles.form__button} type="submit" value="Sign Up" />
       </form>
     </div>
   );
--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -4,107 +4,44 @@ import CompanyRegistration from "../CompanyRegistrationForm/CompanyRegistrationF
 import LogInForm from "../LogInForm/LogInForm";
 
 const LogIn = () => {
+
+
   return (
-      <div className={styles.container}>
-        <div>
-          <div>
-            <div className={styles.form}>
-              <h3 className="mb-0 pb-3">
-                <span>Log In&nbsp;&nbsp;&nbsp;</span>
-                <span>Sign Up</span>
-              </h3>
-              <input
-                className={styles.checkbox}
-                type="checkbox"
-                id="reg-log"
-                name="reg-log"
-              />
-              <label htmlFor="reg-log"></label>
-              <div className={styles.card_3d_wrap}>
-                <div className={styles.card_3d_wrapper}>
-                  <div className={styles.card_front}>
-                    <div className={styles.center_wrap}>
-                      <div className={styles.section}>
-                        <h4 className="mb-4 pb-3 h4-sign">Log In</h4>
-                        {/*<div className="form-group">
-                          <input
-                            type="email"
-                            name="logemail"
-                            className={styles.formStyle}
-                            placeholder="Your Email"
-                            id="logemail"
-                            autoComplete="off"
-                          />
-                        </div>
-                        <div className="form-group mt-2">
-                          <input
-                            type="password"
-                            name="logpass"
-                            className={styles.formStyle}
-                            placeholder="Your Password"
-                            id="logpass-password"
-                            autoComplete="off"
-                          />
-                        </div>
-                        <a href="#" className="btn-sign mt-4 sign-link">
-                          submit
-                        </a>
-                        <p className="mb-0 mt-4 text-center">
-                          <a href="#0" className="link-in sign-link">
-                            Forgot your password?
-                          </a>
-                        </p>*/}
-                        <LogInForm />
-                      </div>
-                    </div>
-                  </div>
-                  <div className={styles.card_back}>
-                    <div className={styles.center_wrap}>
-                      <div className={styles.section}>
-                        <h4 className="mb-4 pb-3 h4-sign">Sign Up</h4>
-                        {/*<div className="form-group">
-                          <input
-                            type="text"
-                            name="logname"
-                            className={styles.formStyle}
-                            placeholder="Your Full Name"
-                            id="logname"
-                            autoComplete="off"
-                          />
-                        </div>
-                        <div className="form-group mt-2">
-                          <input
-                            type="email"
-                            name="logemail"
-                            className={styles.formStyle}
-                            placeholder="Your Email"
-                            id="autocomplete"
-                            autoComplete="off"
-                          />
-                        </div>
-                        <div className="form-group mt-2">
-                          <input
-                            type="password"
-                            name="logpass"
-                            className={styles.formStyle}
-                            placeholder="Your Password"
-                            id="logpass-autocomplete"
-                            autoComplete="off"
-                          />
-                        </div>
-                        <a href="#" className="btn-sign mt-4 sign-link">
-                          submit
-                        </a>*/}
-                        <CompanyRegistration/>
-                      </div>
-                    </div>
-                  </div>
+    <div className={styles.container}>
+      <div className={styles.form}>
+        <h3 className="mb-0 pb-3">
+          <span>Log In&nbsp;&nbsp;&nbsp;</span>
+          <span>Sign Up</span>
+        </h3>
+        <input
+          className={styles.checkbox}
+          type="checkbox"
+          id="reg-log"
+          name="reg-log"
+        />
+        <label htmlFor="reg-log"></label>
+        <div className={styles.card_3d_wrap}>
+          <div className={styles.card_3d_wrapper}>
+            <div className={styles.card_front}>
+              <div className={styles.center_wrap}>
+                <div className={styles.section}>
+                  <h4 className="mb-4 pb-3 h4-sign">Log In</h4>
+                  <LogInForm />
+                </div>
+              </div>
+            </div>
+            <div className={styles.card_back}>
+              <div className={styles.center_wrap}>
+                <div className={styles.section}>
+                  <h4 className="mb-4 pb-3 h4-sign">Sign Up</h4>
+                  <CompanyRegistration />
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
+    </div>
   );
 };
 
