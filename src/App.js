c96db3a91ab58924db93b2b2c7373bcf11b3d65a
Efim Shikanov <darkcoderb@gmail.com>
2022-10-13 17:39:05 +0500

    fixed error

--- src/App.js
+++ src/App.js
@@ -23,10 +23,6 @@ function App(props) {
   }, []);
 
   useEffect(() => {
-    authService.getMemberInformation(props.setMember);
-  }, []);
-
-  useEffect(() => {
     if (!props.member) {
       props.openLogIn();
     }
